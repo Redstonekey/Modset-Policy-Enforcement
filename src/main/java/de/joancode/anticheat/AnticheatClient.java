024d853d569125a27189a3b544e4df94c61b1c48
Joan Code <none>
2025-08-21 21:18:37 +0200

    fixing bugs

--- src/main/java/de/joancode/anticheat/AnticheatClient.java
+++
@@ -1,7 +0,0 @@
-package de.joancode.anticheat;
-
-/**
- * Server environment stub (real implementation is in the client source set at src/client/java).
- * This prevents server (main) source compilation from depending on client-only Fabric APIs.
- */
-public final class AnticheatClient { /* no-op stub */ }
--- src/main/java/de/joancode/anticheat/util/BannedModsUpdater.java
+++
@@ -1,6 +0,0 @@
-package de.joancode.anticheat.util;
-
-/**
- * Server environment stub (real implementation lives under client source set).
- */
-public final class BannedModsUpdater { /* no-op stub */ }
